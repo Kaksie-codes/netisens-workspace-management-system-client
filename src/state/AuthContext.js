16d933c6bcb77a3c30e8e662a0710d689adb5dce
Nsikak Akpan <builtbykaksy@gmail.com>
2025-01-17 10:58:03 +0100

    updates

---
+++ src/state/AuthContext.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
